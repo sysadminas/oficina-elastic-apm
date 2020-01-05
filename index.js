4231fc783b16291a42375e486fdca6f76615feb0
Marylly Araújo Silva <mymarylly@gmail.com>
2020-01-05 14:30:01 -0300

    Linter fixes

--- index.js
+++ index.js
@@ -3,33 +3,32 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    server.route([
-        {
-            method: 'GET',
-            path: '/hello', handler: (request, h) => {
-                return 'Hello World!';
-            }
-        },
-        {
-            method: 'GET',
-            path: '/byebye', handler: (request, h) => {
-                return 'Bye bye'
-            }
-        }
-    ]);
+  server.route([
+    {
+      method: 'GET',
+      path: '/hello',
+      handler: () => 'Hello World!',
+    },
+    {
+      method: 'GET',
+      path: '/byebye',
+      handler: () => 'Bye bye',
+    },
+  ]);
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
+
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
